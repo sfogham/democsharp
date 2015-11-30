bfed9001d87d76684251e870db95887aa6ed73cc
DOBIGNARD Nicolas <nicolas.dobignard@docapost.fr>
2015-11-30 11:53:35 +0100

    Commit de la branche dev

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world !!!");
-            Console.WriteLine("Hello world from dev branch !!!");
+            Console.WriteLine("Hello world from dev branch!!!");
         }
     }
 }
