9a7ba5bc1d4967f7de765718f7398f87b01d2fef
DOBIGNARD Nicolas <nicolas.dobignard@docapost.fr>
2015-11-30 11:39:41 +0100

    First commit on the branch!

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world !!!");
+            Console.WriteLine("Hello world from dev branch !!!");
         }
     }
 }
